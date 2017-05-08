40049ea70ec0646784bd6ae335026779ad0e5ac1
ankita saini <as3323@afsconnect1.njit.edu>
2017-05-08 15:24:18 -0400

    header added

---
+++ todoheader.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ToDo App</title>
+        <link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="main.css">
+	        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+		    <script
+		    src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		    </head>
+		    <body>
+		    <h2>Todo App</h2>
+		    </div>
+		    
+
